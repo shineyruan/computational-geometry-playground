c45957624668790ffa216e3d22c87d0c53bcbe1c
Zhihao Ruan <ruanzh@seas.upenn.edu>
2022-07-28 02:09:43 -0700

    add demo code for main

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,23 @@
 
 #include <spdlog/spdlog.h>
 
+#include "visualization/drawable/line_string.hpp"
+#include "visualization/drawable/point_set.hpp"
+#include "visualization/visualizer/opencv_visualizer.hpp"
+
+using namespace cgzr;
+
 int main() {
   spdlog::info("Hello!");
+
+  visualization::OpenCVVisualizer visualizer;
+  visualization::LineString line_string;
+  visualization::PointSet point_set;
+
+  visualizer.Add(&line_string);
+  visualizer.Add(&point_set);
+
+  spdlog::info("Bye!");
+
   return 0;
 }
