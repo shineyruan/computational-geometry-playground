c672eb741a7a38601f136097722f2110a23aa942
Zhihao Ruan <ruanzh@seas.upenn.edu>
2022-07-28 06:01:20 -0400

    add define guard for macOS

--- src/utils/include_eigen.hpp
+++ src/utils/include_eigen.hpp
@@ -1,9 +1,12 @@
 #ifndef __UTILS_EIGEN_INCLUDE__HPP__
 #define __UTILS_EIGEN_INCLUDE__HPP__
 
+// only for M1 Mac's VSCode intellisense completion
+#ifdef __OSX__
 #if __INTELLISENSE__
 #undef __ARM_NEON
 #undef __ARM_NEON__
+#endif
 #endif
 
 #include <Eigen/Dense>
