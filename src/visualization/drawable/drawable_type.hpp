1247d0d451cbe4dc556f86f46218f19b0858ec89
Zhihao Ruan <ruanzh@seas.upenn.edu>
2022-07-28 02:08:42 -0700

    implement Drawable type cast to string

--- src/visualization/drawable/drawable_type.hpp
+++ src/visualization/drawable/drawable_type.hpp
@@ -1,8 +1,34 @@
 #ifndef __VISUALIZATION_DRAWABLE_DRAWABLE_TYPE__HPP__
 #define __VISUALIZATION_DRAWABLE_DRAWABLE_TYPE__HPP__
 
+#include <string>
+
 namespace cgzr {
-enum class DrawableType { POINT_SET, LINE_STRING, UNSUPPORTED };
+enum class DrawableType { POINT_SET = 0, LINE_STRING, UNSUPPORTED };
+
+inline std::string ToString(DrawableType type) {
+  std::string ret;
+
+  switch (type) {
+    case DrawableType::POINT_SET:
+      ret = "point set";
+      break;
+
+    case DrawableType::LINE_STRING:
+      ret = "line string";
+      break;
+
+    case DrawableType::UNSUPPORTED:;
+      ret = "unsupported";
+      break;
+
+    default:
+      ret = "unknown";
+      break;
+  }
+
+  return ret;
 }
+}  // namespace cgzr
 
 #endif /* __VISUALIZATION_DRAWABLE_DRAWABLE_TYPE__HPP__ */
