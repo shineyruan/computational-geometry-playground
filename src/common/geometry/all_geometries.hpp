39281ddd94612454ad19fe5a479fd2f2c725be0e
Zhihao Ruan <ruanzh@seas.upenn.edu>
2022-10-15 04:32:07 -0700

    add header file to include all existing geometries

---
+++ src/common/geometry/all_geometries.hpp
@@ -0,0 +1,7 @@
+#ifndef __COMMON_GEOMETRY_ALL_GEOMETRIES__HPP__
+#define __COMMON_GEOMETRY_ALL_GEOMETRIES__HPP__
+
+#include "lines2d.hpp"
+#include "points2d.hpp"
+
+#endif /* __COMMON_GEOMETRY_ALL_GEOMETRIES__HPP__ */
