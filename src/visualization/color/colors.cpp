d423731788c81f2176dd7c02c8bbbd6e5c0f7109
Zhihao Ruan <ruanzh@seas.upenn.edu>
2022-10-24 02:06:29 -0700

    predefine several color options

---
+++ src/visualization/color/colors.cpp
@@ -0,0 +1,15 @@
+#include "colors.hpp"
+
+namespace cgzr {
+namespace colors {
+const ColorRGB COLOR_BLACK(0., 0., 0.);
+const ColorRGB COLOR_WHITE(255., 255., 255.);
+const ColorRGB COLOR_RED(255., 0., 0.);
+const ColorRGB COLOR_GREEN(0., 255., 0.);
+const ColorRGB COLOR_BLUE(0., 0., 255.);
+const ColorRGB COLOR_YELLOW(255., 255., 0);
+const ColorRGB COLOR_CYAN(0., 255., 255.);
+const ColorRGB COLOR_MAGENTA(255., 0., 255.);
+}  // namespace colors
+
+}  // namespace cgzr
--- src/visualization/color/colors.hpp
+++ src/visualization/color/colors.hpp
@@ -11,6 +11,18 @@ struct ColorRGB {
   ColorRGB(double new_r, double new_g, double new_b)
       : r(new_r), g(new_g), b(new_b) {}
 };
+
+namespace colors {
+extern const ColorRGB COLOR_BLACK;
+extern const ColorRGB COLOR_WHITE;
+extern const ColorRGB COLOR_RED;
+extern const ColorRGB COLOR_GREEN;
+extern const ColorRGB COLOR_BLUE;
+extern const ColorRGB COLOR_YELLOW;
+extern const ColorRGB COLOR_CYAN;
+extern const ColorRGB COLOR_MAGENTA;
+}  // namespace colors
+
 }  // namespace cgzr
 
 #endif /* __VISUALIZATION_COLOR_COLORS__HPP__ */
