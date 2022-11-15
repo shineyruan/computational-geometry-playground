474db488cbd11ec6adbdc0adad5312efe2a3db11
Zhihao Ruan <ruanzh@seas.upenn.edu>
2022-11-15 11:36:59 -0800

    finish convex hull algorithm

--- src/algorithm/solver/convex_hull_solver.cpp
+++ src/algorithm/solver/convex_hull_solver.cpp
@@ -2,6 +2,8 @@
 
 #include <spdlog/spdlog.h>
 
+#include <list>
+
 #include "common/geometry/lines2d.hpp"
 
 namespace cgzr {
@@ -68,10 +70,79 @@ std::vector<std::unique_ptr<GeometryBase>> ConvexHullSolver::Solve(
       points.begin(), points.end(),
       [](const Point2d& p1, const Point2d& p2) { return p1.x() < p2.x(); });
 
-  // TODO: 2. Put p1, p2 in a list L_upper
-  std::vector<Point2d> L_upper{points[0], points[1]};
+  // 2. Construct the upper hull
+  std::list<Point2d> L_upper{points[0], points[1]};
+  for (size_t i = 2; i < points.size(); ++i) {
+    //  2.1 append p_i to L_upper
+    L_upper.push_back(points[i]);
+    //  2.2 while L_upper has more than 2 points and the last 3 does not make a
+    //     right turn
+    while (L_upper.size() > 2U && !IsRightTurn(*std::prev(L_upper.end(), 3),
+                                               *std::prev(L_upper.end(), 2),
+                                               *std::prev(L_upper.end(), 1))) {
+      // delete the middle of the last 3 points
+      L_upper.erase(std::prev(L_upper.end(), 2));
+    }
+  }
+
+  // 3. Construct the lower hull
+  std::list<Point2d> L_lower{*std::prev(points.end(), 1),
+                             *std::prev(points.end(), 2)};
+  for (int i = static_cast<int>(points.size() - 3); i >= 0; i--) {
+    // 3.1 append p_i to L_lower
+    L_lower.push_back(points[i]);
+    // 3.2 while L_lower contains more than 2 points and the last 3 does not
+    //    make a right turn
+    while (L_lower.size() > 2U && !IsRightTurn(*std::prev(L_lower.end(), 3),
+                                               *std::prev(L_lower.end(), 2),
+                                               *std::prev(L_lower.end(), 1))) {
+      // delete the middle of the last 3 points
+      L_lower.erase(std::prev(L_lower.end(), 2));
+    }
+  }
+
+  // 4. Remove the 1st and last point from L_lower to avoid duplication at where
+  // the upper & lower hull meet
+  assert(L_lower.size() >= 2U);
+  L_lower.erase(L_lower.begin());
+  L_lower.erase(std::prev(L_lower.end()));
+
+  // 5. Append L_lower to L_upper, and form L
+  std::vector<Point2d> L{L_upper.begin(), L_upper.end()};
+  L.insert(L.end(), L_lower.begin(), L_lower.end());
+
+  // 6. Construct return
+  //
+  // 6.1 construct points
+  ret.emplace_back(std::make_unique<Points2d>(L));
+
+  // 6.2 construct connecting lines
+  auto lines_2d = GeometryBase::Create(GeometryType::LINES_2D);
+  Lines2D* p_lines = dynamic_cast<Lines2D*>(lines_2d.get());
+
+  std::vector<std::pair<size_t, size_t>> line_indices{};
+  line_indices.reserve(L.size());
+  for (size_t i = 0; i < L.size(); ++i) {
+    if (i == L.size() - 1) {
+      line_indices.emplace_back(i, 0U);
+    } else {
+      line_indices.emplace_back(i, i + 1);
+    }
+  }
+
+  p_lines->UpdatePoints(L);
+  p_lines->UpdateLineAsIndices(line_indices);
+  ret.emplace_back(std::move(lines_2d));
 
   return ret;
+}
+
+bool ConvexHullSolver::IsRightTurn(const Eigen::Vector2d& p1,
+                                   const Eigen::Vector2d& p2,
+                                   const Eigen::Vector2d& p3) const {
+  Eigen::Vector3d v1{p2.x() - p1.x(), p2.y() - p1.y(), 0.};
+  Eigen::Vector3d v2{p3.x() - p2.x(), p3.y() - p2.y(), 0.};
+  return v1.cross(v2).z() < std::numeric_limits<double>::epsilon();
 }
 
 }  // namespace cgzr
--- src/algorithm/solver/convex_hull_solver.hpp
+++ src/algorithm/solver/convex_hull_solver.hpp
@@ -12,6 +12,10 @@ public:
 
   std::vector<std::unique_ptr<GeometryBase>> Solve(
       const std::vector<const GeometryBase*>& in_geometries) const override;
+
+private:
+  bool IsRightTurn(const Eigen::Vector2d& p1, const Eigen::Vector2d& p2,
+                   const Eigen::Vector2d& p3) const;
 };
 
 }  // namespace cgzr
