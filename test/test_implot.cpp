cee8f5c6956f6c619309d0bb9fa5e2b1b38f156a
Zhihao Ruan <shineyruan@gmail.com>
2023-04-07 00:03:53 -0700

    revert to `rand()` for macOS

--- test/test_implot.cpp
+++ test/test_implot.cpp
@@ -100,7 +100,7 @@ namespace ImPlot {
 
 template <typename T>
 inline T RandomRange(T min, T max) {
-  T scale = rand_r() / (T)RAND_MAX;
+  T scale = rand() / (T)RAND_MAX;
   return min + scale * (max - min);
 }
 
