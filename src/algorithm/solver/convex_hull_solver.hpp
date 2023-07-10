2d014e3d7b782fa75527138a19c95000fe22814c
Zhihao Ruan <shineyruan@gmail.com>
2023-07-09 17:31:39 -0700

    feat(convex_hull): add documentation (#28)
    add documentation to ConvexHullSolver

--- src/algorithm/solver/convex_hull_solver.hpp
+++ src/algorithm/solver/convex_hull_solver.hpp
@@ -6,14 +6,24 @@
 
 namespace cgzr {
 
+/**
+ * @brief Given a set of points in 2D, find the convex hull of all the points
+ * representing in {vertices, line_strings} form.
+ */
 class ConvexHullSolver : public SolverBase {
  public:
   ConvexHullSolver() : SolverBase(SolverType::CONVEX_HULL) {}
 
+  /**
+   * @param in_geometries   A set of points. Should only contain {points_2d}
+   * @return  A convex hull representation. Should only contain {points_2d,
+   * lines_2d}
+   */
   std::vector<std::unique_ptr<GeometryBase>> Solve(
       const std::vector<const GeometryBase*>& in_geometries) const override;
 
  private:
+  // Checks whether p2-p1 --> p3-p2 forms a right turn
   bool IsRightTurn(const Eigen::Vector2d& p1, const Eigen::Vector2d& p2,
                    const Eigen::Vector2d& p3) const;
 };
