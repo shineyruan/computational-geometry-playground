c5a4acbc32ed31b6a2f595cfc394fd2284e6ac48
Zhihao Ruan <shineyruan@gmail.com>
2023-08-18 02:53:35 -0700

    add utilities & rename Lines2d

--- src/common/geometry/lines2d.hpp
+++ src/common/geometry/lines2d.hpp
@@ -5,14 +5,17 @@
 
 namespace cgzr {
 
-class Lines2D : public GeometryBase {
+class Lines2d : public GeometryBase {
  public:
-  Lines2D() : GeometryBase(GeometryType::LINES_2D) {}
-  Lines2D(const std::vector<Eigen::Vector2d>& endpoints,
+  Lines2d() : GeometryBase(GeometryType::LINES_2D) {}
+  Lines2d(const std::vector<Eigen::Vector2d>& endpoints,
           const std::vector<std::pair<size_t, size_t>>& lines)
       : GeometryBase(GeometryType::LINES_2D),
         endpoints_(endpoints),
         lines_(lines) {}
+
+  // random initialization with designated size
+  Lines2d(size_t num_lines);
 
   size_t Size() const override { return lines_.size(); }
   std::unique_ptr<GeometryBase> Clone() const override;
@@ -21,6 +24,8 @@ class Lines2D : public GeometryBase {
 
   void UpdatePoints(const std::vector<Eigen::Vector2d>& endpoints);
   bool UpdateLineAsIndices(const std::vector<std::pair<size_t, size_t>>& lines);
+
+  std::vector<std::pair<Eigen::Vector2d, Eigen::Vector2d>> Lines() const;
 
  private:
   std::vector<Eigen::Vector2d> endpoints_;
