dd9539fba195a0a232549382e8bed570d45db453
Zhihao Ruan <shineyruan@gmail.com>
2023-08-18 02:53:59 -0700

    implement data extraction & type definitions

--- src/algorithm/solver/line_segment_intersection_solver.cpp
+++ src/algorithm/solver/line_segment_intersection_solver.cpp
@@ -2,6 +2,8 @@
 
 #include <spdlog/spdlog.h>
 
+#include <map>
+
 #include "common/geometry/lines2d.hpp"
 #include "common/geometry/points2d.hpp"
 
@@ -9,8 +11,8 @@ namespace cgzr {
 
 namespace {
 // For line segments, there must be only one input geometry, and it must be
-// Lines2D.
-const Lines2D* ExtractInput(
+// Lines2d.
+const Lines2d* ExtractInput(
     const std::vector<const GeometryBase*>& in_geometries) {
   // 1. Check input size
   if (in_geometries.size() != 1) {
@@ -18,7 +20,7 @@ const Lines2D* ExtractInput(
     return nullptr;
   }
   // 2. Check if it's Lines2d
-  auto lines_2d = dynamic_cast<const Lines2D*>(in_geometries.front());
+  auto lines_2d = dynamic_cast<const Lines2d*>(in_geometries.front());
   if (!lines_2d) {
     SPDLOG_ERROR("input {} is not correct type {}",
                  ToString(GeometryType::LINES_2D),
@@ -44,6 +46,23 @@ std::vector<std::unique_ptr<GeometryBase>> LineSegmentIntersectionSolver::Solve(
   if (!lines_2d) {
     return result;
   }
+
+  using Point2d = Eigen::Vector2d;
+  using Segment2d = std::pair<Eigen::Vector2d, Eigen::Vector2d>;
+
+  //  0.1 construct line segment from Lines2d
+  std::vector<Segment2d> segments;
+  auto lines = lines_2d->Lines();
+  for (const auto& [p1, p2] : lines) {
+    segments.emplace_back(p1, p2);
+  }
+
+  // 1. Initialize event queue
+  // Event queue is a mapping from segment endpoints to pointer to segments
+  auto point_cmp = [](const Point2d& p1, const Point2d& p2) {
+    return p1.y() > p2.y() || (p1.y() == p2.y() && p1.x() < p2.x());
+  };
+  std::map<Point2d, const Segment2d*, decltype(point_cmp)> event_q(point_cmp);
 
   return result;
 }
