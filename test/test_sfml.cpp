94528fbc221511e068eb07061dbf714a0885a2c4
Zhihao Ruan <shineyruan@gmail.com>
2025-07-18 23:32:32 -0700

    fix test compile error

--- test/test_sfml.cpp
+++ test/test_sfml.cpp
@@ -6,6 +6,8 @@
  */
 
 #include <SFML/Graphics.hpp>
+#include <SFML/System/Vector2.hpp>
+#include <optional>
 
 int main() {
   // ----------- Create Shapes -----------
@@ -14,7 +16,7 @@ int main() {
   circle.setRadius(100.f);
   circle.setPointCount(400);
   circle.setFillColor(sf::Color(150, 50, 250));
-  circle.move(50.f, 50.f);
+  circle.move(sf::Vector2f{50.f, 50.f});
   // set a 10-pixel wide orange outline
   circle.setOutlineThickness(10.f);
   circle.setOutlineColor(sf::Color(250, 150, 100));
@@ -25,31 +27,32 @@ int main() {
   // change the size to 100x100
   rectangle.setSize(sf::Vector2f(200.f, 100.f));
   rectangle.setFillColor(sf::Color(0, 255, 0));
-  rectangle.move(400.f, 400.f);
+  rectangle.move(sf::Vector2f{400.f, 400.f});
 
   // create a line
-  sf::Vertex line[] = {sf::Vertex(sf::Vector2f(10.f, 10.f)),
-                       sf::Vertex(sf::Vector2f(150.f, 150.f))};
+  sf::Vertex line[] = {sf::Vertex{sf::Vector2f{10.f, 10.f}},
+                       sf::Vertex{sf::Vector2f{150.f, 150.f}}};
 
   // ----------- Create Window -----------
   sf::ContextSettings settings;
-  settings.antialiasingLevel = 8;
+  settings.antiAliasingLevel = 8;
   // create the window
-  sf::RenderWindow window(sf::VideoMode(800, 600), "Visualization",
-                          sf::Style::Close, settings);
+  sf::RenderWindow window(sf::VideoMode(sf::Vector2u{800, 600}),
+                          "Visualization", sf::State::Windowed, settings);
 
   // ----------- Main Loop -----------
   // run the program as long as the window is open
   while (window.isOpen()) {
     // check all the window's events that were triggered since the last
     // iteration of the loop
-    sf::Event event;
-    while (window.pollEvent(event)) {
+    while (const std::optional<sf::Event> event = window.pollEvent()) {
       // "close requested" event: we close the window
-      bool button_close = event.type == sf::Event::Closed;
-      bool key_q_or_esc = event.type == sf::Event::KeyPressed &&
-                          (event.key.code == sf::Keyboard::Q ||
-                           event.key.code == sf::Keyboard::Escape);
+      bool button_close = event->is<sf::Event::Closed>();
+      bool key_q_or_esc = event->is<sf::Event::KeyPressed>() &&
+                          (event->getIf<sf::Event::KeyPressed>()->code ==
+                               sf::Keyboard::Key::Q ||
+                           event->getIf<sf::Event::KeyPressed>()->code ==
+                               sf::Keyboard::Key::Escape);
       if (button_close || key_q_or_esc) {
         window.close();
       }
@@ -61,7 +64,7 @@ int main() {
     // draw everything here...
     window.draw(circle);
     window.draw(rectangle);
-    window.draw(line, 2, sf::Lines);
+    window.draw(line, 2, sf::PrimitiveType::Lines);
 
     // end the current frame
     window.display();
